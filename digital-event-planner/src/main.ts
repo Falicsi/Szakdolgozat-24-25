7c03d0c2142b51637af5ad2c74648de1d18fdcdc
Béres Ákos <h043580@stud.u-szeged.hu>
2025-05-20 17:58:13 +0200

    main.ts + register, login firebase

--- digital-event-planner/src/main.ts
+++ digital-event-planner/src/main.ts
@@ -7,6 +7,10 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { CalendarModule, DateAdapter } from 'angular-calendar';
 import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
 import { AuthInterceptor } from './app/interceptors/auth.interceptor';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from './environments/environment';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -18,10 +22,13 @@ bootstrapApplication(AppComponent, {
         useFactory: adapterFactory
       })
     ),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
       multi: true
-    } 
+    }
   ]
 }).catch((err) => console.error(err));
